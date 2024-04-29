4d12c4459b30df2bb684b7c13cac34aabb08c7b2
Robbie <robbie331@>
2024-04-29 16:22:41 +0900

    1 lv - 5 결과 10개를 저장 할 수 있는 배열 선언 및 생성 연산 결과 저장

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,10 +6,14 @@ public class App {
 
   public static void main(String[] args) {
 
+    int[] arr = new int[10];
+    int index = 0;
+
     Scanner sc = new Scanner(System.in);
     System.out.print("첫 번째 숫자를 입력하세요: ");
     int result = sc.nextInt();
-    while (true) {
+
+    while (index != 10) {
       System.out.print("숫자를 입력하세요: ");
       int secondNumber = sc.nextInt();
       System.out.print("사칙연산 기호를 입력하세요: ");
@@ -24,11 +28,8 @@ public class App {
         default -> throw new IllegalStateException("사칙 연산 기호가 '" + operator + "' 아닙니다. ");
       };
       System.out.println("결과: " + result);
-      System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
-      String answer = sc.next();
-      if (answer.equals("exit")) {
-        break;
-      }
+      arr[index++] = result;
     }
+
   }
 }
