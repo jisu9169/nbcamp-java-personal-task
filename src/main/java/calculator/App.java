a527070f7ae83ba87c6b96e2d92291467ff6522b
Robbie <robbie331@>
2024-04-29 16:40:36 +0900

    1 lv - 8 “inquiry”라는 문자열이 입력되면 저장된 연산 결과 전부를 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,7 +8,7 @@ public class App {
 
   public static void main(String[] args) {
 
-    Queue<Integer> que = new LinkedList<Integer>();
+    Queue<Integer> que = new LinkedList<>();
 
     Scanner sc = new Scanner(System.in);
     System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -30,20 +30,26 @@ public class App {
       };
       que.offer(result);
       System.out.println("결과: " + result);
-      System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+      System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)  ");
       String remove = sc.next();
       if (remove.equals("remove")) {
         System.out.println(que.poll() + " =  최근 값 삭제");
       }
-      System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
+      System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)  ");
+      String inquiry = sc.next();
+      if (inquiry.equals("inquiry")) {
+        while (!que.isEmpty()) {
+          System.out.print("  " + que.poll());
+        }
+        System.out.println();
+      }
+      System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)  ");
       String answer = sc.next();
       if (answer.equals("exit")) {
         break;
       }
     }
+
     sc.close();
-    while (!que.isEmpty()) {
-      System.out.print("  " + que.poll());
-    }
   }
 }
