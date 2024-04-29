a8c57ca0ba69eb4afdf27abf9e940c9f5d2c7adf
Robbie <robbie331@>
2024-04-29 17:12:41 +0900

    2 lv - 1 calculate 매서드 구현 및 생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,25 @@
 package calculator;
 
-public class Calculator {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Calculator {
+  List<Double> li = new ArrayList<>();
+
+  public double calculate(int firstNumber, int secondNumber, char operator) throws IllegalAccessException {
+    if(operator == '+') {
+      return firstNumber + secondNumber;
+    }else if(operator == '-') {
+      return firstNumber - secondNumber;
+    }else if(operator == '*') {
+      return firstNumber * secondNumber;
+    }else if(operator == '/') {
+      if(secondNumber == 0) {
+        throw new IllegalAccessException(" 분모 값을" + secondNumber + " 잘못 입력하셨습니다.");
+      }
+      return (double) firstNumber / secondNumber;
+    }else{
+      throw new IllegalAccessException("사칙 연산값이 " + operator+" 로 아닙니다.");
+    }
+  }
 }
