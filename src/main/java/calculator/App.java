2e22cddf0705042f4e7427603d66075ef287f561
Robbie <robbie331@>
2024-04-29 16:28:23 +0900

    1 lv - 6 연산된 결과 10개 넘으면 저장된 결과 삭제하고 새로운 연산 결과 저장

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -13,7 +13,7 @@ public class App {
     System.out.print("첫 번째 숫자를 입력하세요: ");
     int result = sc.nextInt();
 
-    while (index != 10) {
+    while (true) {
       System.out.print("숫자를 입력하세요: ");
       int secondNumber = sc.nextInt();
       System.out.print("사칙연산 기호를 입력하세요: ");
@@ -27,9 +27,16 @@ public class App {
 
         default -> throw new IllegalStateException("사칙 연산 기호가 '" + operator + "' 아닙니다. ");
       };
-      System.out.println("결과: " + result);
-      arr[index++] = result;
+      arr[index++ % 10] = result;
+      System.out.println(index + "번째 계산 결과: " + result);
+      System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
+      String answer = sc.next();
+      if (answer.equals("exit")) {
+        break;
+      }
     }
-
+    for (int i : arr) {
+      System.out.print(i + " ");
+    }
   }
 }
