e95885e0a96e1f55f0f39a000f7def575f17309c
Robbie <robbie331@>
2024-04-29 15:11:15 +0900

    file commit

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,14 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int firstNumber = sc.nextInt();
+        int secondNumber = sc.nextInt();
 
     }
 }
