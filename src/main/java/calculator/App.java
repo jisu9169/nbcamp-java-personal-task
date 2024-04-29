a29035ca12efc72fa18ae6c35fe3cb42d3cbce9a
Robbie <robbie331@>
2024-04-29 17:22:39 +0900

    2 lv - 2 while 계산 값들 저장

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,55 +1,38 @@
 package calculator;
 
-import java.util.LinkedList;
-import java.util.Queue;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-  public static void main(String[] args) {
+  static List<Integer> result;
+  public static void main(String[] args) throws IllegalAccessException {
 
-    Queue<Integer> que = new LinkedList<>();
+    Calculator calculator = new Calculator();
 
     Scanner sc = new Scanner(System.in);
-    System.out.print("첫 번째 숫자를 입력하세요: ");
-    int result = sc.nextInt();
 
     while (true) {
-      System.out.print("숫자를 입력하세요: ");
-      int secondNumber = sc.nextInt();
-      System.out.print("사칙연산 기호를 입력하세요: ");
-      String operator = sc.next();
-      result = switch (operator) {
-        case "+" -> result + secondNumber;
-        case "-" -> result - secondNumber;
-        case "*" -> result * secondNumber;
-        case "/" -> result / secondNumber;
-        case "%" -> result % secondNumber;
+      System.out.print("첫 번째 숫자를 입력하세요:");
+      int num1 = sc.nextInt();
+      System.out.print("두 번째 숫자를 입력하세요:");
+      int num2 = sc.nextInt();
 
-        default -> throw new IllegalStateException("사칙 연산 기호가 '" + operator + "' 아닙니다. ");
-      };
-      que.offer(result);
-      System.out.println("결과: " + result);
-      System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)  ");
-      String remove = sc.next();
-      if (remove.equals("remove")) {
-        System.out.println(que.poll() + " =  최근 값 삭제");
-      }
-      System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)  ");
-      String inquiry = sc.next();
-      if (inquiry.equals("inquiry")) {
-        while (!que.isEmpty()) {
-          System.out.print("  " + que.poll());
-        }
-        System.out.println();
-      }
-      System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)  ");
-      String answer = sc.next();
-      if (answer.equals("exit")) {
+      System.out.print("사칙연산 기호를 입력하세요: ");
+      char operator = sc.next().charAt(0);
+
+      result = calculator.calculate(num1, num2, operator);
+
+      System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+      String exit = sc.next();
+      if (exit.equals("exit")) {
         break;
       }
     }
-
     sc.close();
+    for (Integer r: result) {
+      System.out.print(r+ " ");
+    }
   }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,22 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-  List<Double> li = new ArrayList<>();
 
-  public double calculate(int firstNumber, int secondNumber, char operator) throws IllegalAccessException {
-    if(operator == '+') {
-      return firstNumber + secondNumber;
-    }else if(operator == '-') {
-      return firstNumber - secondNumber;
-    }else if(operator == '*') {
-      return firstNumber * secondNumber;
-    }else if(operator == '/') {
-      if(secondNumber == 0) {
+  List<Integer> li = new ArrayList<>();
+
+  public List<Integer> calculate(int firstNumber, int secondNumber, char operator)
+      throws IllegalAccessException {
+    if (operator == '+') {
+      li.add((firstNumber + secondNumber));
+    } else if (operator == '-') {
+      li.add((firstNumber - secondNumber));
+    } else if (operator == '*') {
+      li.add((firstNumber * secondNumber));
+    } else if (operator == '/') {
+      if (secondNumber == 0) {
         throw new IllegalAccessException(" 분모 값을" + secondNumber + " 잘못 입력하셨습니다.");
       }
-      return (double) firstNumber / secondNumber;
-    }else{
-      throw new IllegalAccessException("사칙 연산값이 " + operator+" 로 아닙니다.");
+      li.add( (firstNumber / secondNumber));
+    } else {
+      throw new IllegalAccessException("사칙 연산값이 " + operator + " 로 아닙니다.");
     }
+    return li;
   }
 }
